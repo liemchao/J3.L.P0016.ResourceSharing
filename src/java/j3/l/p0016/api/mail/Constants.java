6f0e4572ce8f6b097ab4f5f8e41450019868a613
TUNGPT <MyLifeUnderMyControl@gmail.com>
2021-08-16 17:24:26 +0700

    LoginMail Xong

---
+++ src/java/j3/l/p0016/api/mail/Constants.java
@@ -0,0 +1,24 @@
+package j3.l.p0016.api.mail;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author TUNGPT
+ */
+public class Constants {
+    public static String GOOGLE_CLIENT_ID = "892887800803-ijj5g63gm917cqotp3sdng12u01g1tru.apps.googleusercontent.com";
+    public static String GOOGLE_CLIENT_SECRET = "VJW3evHDGFMwAItzFYBfm281";
+    public static String GOOGLE_REDIRECT_URI = "http://localhost:8084/J3.L.P0016.ResourceSharing/LoginMailServlet";
+    public static String GOOGLE_LINK_GET_TOKEN = "https://oauth2.googleapis.com/token";
+    public static String GOOGLE_LINK_GET_USER_INFO = "https://www.googleapis.com/oauth2/v1/userinfo?access_token=";
+    public static String GOOGLE_GRANT_TYPE = "authorization_code";
+
+}
+//{"web":{"client_id":"63114542360-7ervj769r5dbuk50o78a2cc1sr58r5b8.apps.googleusercontent.com",
+//"project_id":"loginmail-321712","auth_uri":"https://accounts.google.com/o/oauth2/auth",
+//"token_uri":"https://oauth2.googleapis.com/token","auth_provider_x509_cert_url":"https://www.googleapis.com/oauth2/v1/certs","client_secret":"MxVB9zzIXkRyDfEtiecqQGVp","redirect_uris":["http://localhost:8080/LoginMail"],"javascript_origins":["http://localhost:8080"]}}
---
+++ src/java/j3/l/p0016/servlet/LoginMailServlet.java
@@ -0,0 +1,93 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package j3.l.p0016.servlet;
+
+import j3.l.p0016.GoogleDTO.GooglePojo;
+import j3.l.p0016.api.mail.GoogleUtils;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author TUNGPT
+ */
+public class LoginMailServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+       try (PrintWriter out = response.getWriter()) {
+            String code = request.getParameter("code");
+            if (code == null || code.isEmpty()) {
+            RequestDispatcher dis = request.getRequestDispatcher("login.html");
+            dis.forward(request, response);
+            } else {
+                String accessToken = GoogleUtils.getToken(code);
+                GooglePojo googlePojo = GoogleUtils.getUserInfo(accessToken);
+                request.setAttribute("id", googlePojo.getId());
+                request.setAttribute("name", googlePojo.getName());
+                request.setAttribute("email", googlePojo.getEmail());
+                System.out.println(request.getAttribute("email"));
+                RequestDispatcher dis = request.getRequestDispatcher("success.jsp");
+                dis.forward(request, response);
+            }
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
